1dcea00b379741107671b9786feb916a6f8ab4d7
radupopa2010 <radupopa2010@yahoo.com>
2015-12-01 20:17:38 -0800

    Update finance-calcx.js

--- finance-calculator/finance-calcx.js
+++ finance-calculator/finance-calcx.js
@@ -1,4 +1,13 @@
-function TaycorCurrencyFormat(inp){inp = parseFloat(inp).toFixed(2);inp += '';x = inp.split('.');x1 = x[0];x2 = x.length > 1 ? '.' + x[1] : '';var rgx = /(\d+)(\d{3})/;while (rgx.test(x1)) {x1 = x1.replace(rgx, '$1' + ',' + '$2');}var ret = x1 + x2;if((''+ret) == 'NaN') return '0.00';return ret;}
+function TaycorCurrencyFormat(inp) {
+	inp = parseFloat(inp).toFixed(2);
+	inp += '';
+	x = inp.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');}
+		var ret = x1 + x2;if((''+ret) == 'NaN') return '0.00';return ret;}
 
 $(window).ready(function (){
 				//' /*	[CL] Calculator Update Method 		*/ + '
